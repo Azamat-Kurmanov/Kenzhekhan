2a7439bf844f56d8ddbffcae3f1a23266c934bb0
azama <a.kurmanov@csi.kz>
2023-06-23 22:19:23 +0600

    Initial commit

---
+++ src/main/java/kz/kenzhekhan/kenzhekhan/KenzhekhanApplication.java
@@ -0,0 +1,13 @@
+package kz.kenzhekhan.kenzhekhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenzhekhanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KenzhekhanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/kenzhekhan/kenzhekhan/KenzhekhanApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kenzhekhan.kenzhekhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenzhekhanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
